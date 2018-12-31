be356e91962aa5ddaf951531a95e3999a936e28b
HayeonP <kite9240@gmail.com>
2018-12-31 16:37:38 +0900

    2018-12-31
    SaveFixedFpsVideoFrames was added

---
+++ SaveFixedFpsVideoFrames/SaveFixedFpsVideoFrames/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <filesystem>
+#include <opencv2\opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+#define SAVE_PATH	"D:\\Users\\VisionWork\\Desktop\\박하연\\DB\\SquenceImage"
+#define VIDEO_PATH	"D:\\Users\\VisionWork\\Desktop\\박하연\\DB\\BlackBox\\bsh-181226\\Road_Video"
+#define TARGET_FPS	1
+
+vector<string> get_filepaths_in_directory(string path);
+
+int main() {
+
+	vector<string> video_paths = get_filepaths_in_directory(VIDEO_PATH);
+
+	for (auto it = video_paths.begin(); it != video_paths.end(); it++) {
+		string filepath = (*it);
+
+		string filename;
+
+		istringstream ss(filepath);
+		while (getline(ss, filename, '\\')) {}
+		ss = istringstream(filename);
+		getline(ss, filename, '.');
+
+		VideoCapture vc(filepath);
+		if (!vc.isOpened()) {
+			cout << "Cannot open video file" << endl;
+			return -1;
+		}
+
+		double fps_origin = vc.get(CV_CAP_PROP_FPS);
+
+		Mat frame;
+
+		int skip_count = 0;
+		int save_count = 0;
+		while (1) {
+			vc >> frame;
+			if (frame.empty())
+				break;
+			else
+			{
+				skip_count++;
+				waitKey(1000 / fps_origin);
+				if (skip_count == fps_origin / TARGET_FPS) {
+					imshow("video", frame);
+					imwrite(SAVE_PATH + string("\\") + filename + "_" + to_string(save_count) + string(".jpg"), frame);
+					skip_count = 0;
+					save_count++;
+				}
+			}
+		}
+	}
+
+	return 0;
+
+}
+
+vector<string> get_filepaths_in_directory(string path) {
+	vector<string> paths;
+
+	for (auto& p1 : std::experimental::filesystem::directory_iterator(path)) {
+		paths.push_back(p1.path().string());
+	}
+
+	return paths;
+}
