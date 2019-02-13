6131b5e5ee8d7158b0b5f4983f35f1fb0b08d30d
HayeonP <kite9240@gmail.com>
2019-02-13 18:30:35 +0900

    2019-02-13 Unnecessary files were removed.

---
+++ YOLODataGenerator/Sources/YOLODataGenerator.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <filesystem>
+#include <vector>
+#include <fstream>
+#include <opencv2\opencv.hpp>
+
+#define SAVE_LOAD_PATH	"D:\\Users\\VisionWork\\Desktop\\박하연\\DB\\SequenceImage\\190124_b"
+//#define SAVE_LOAD_PATH	"D:\\Users\\VisionWork\\Desktop\\박하연\\Projects\\TrainingDataGenerator\\YOLODataRefiner\\YOLODataRefiner\\data\\img"
+#define VIDEO_PATH	"D:\\Users\\VisionWork\\Desktop\\박하연\\DB\\BlackBox\\190124_b"
+//#define VIDEO_PATH	"D:\\Users\\VisionWork\\Desktop\\박하연\\Projects\\TrainingDataGenerator\\YOLODataRefiner\\YOLODataRefiner\\data\\img"
+#define TARGET_FPS	3
+#define SEQ_FLAG 1
+#define TXT_FLAG 1
+
+
+using namespace std;
+using namespace cv;
+
+vector<string> get_filepaths_in_directory(string path);
+string ReplaceAll(std::string &str, const std::string& from, const std::string& to);
+string change_file_extention(string filename, string extention);
+void write_file(string path, vector<string> data);
+
+int main(int argc, char** argv) {
+
+	if (SEQ_FLAG) {
+		// Create Sequence Images
+		vector<string> video_paths = get_filepaths_in_directory(VIDEO_PATH);
+
+		for (auto it = video_paths.begin(); it != video_paths.end(); it++) {
+			string filepath = (*it);
+
+			string filename;
+
+			istringstream ss(filepath);
+			while (getline(ss, filename, '\\')) {}
+			ss = istringstream(filename);
+			getline(ss, filename, '.');
+			cout << filename << endl;
+
+
+			VideoCapture vc;
+			vc.open(filepath);
+
+			if (!vc.isOpened()) {
+				cout << "Cannot open video file" << endl;
+				return -1;
+			}
+
+			double fps_origin = vc.get(CV_CAP_PROP_FPS);
+
+			Mat frame;
+			int skip_count = 0;
+			int save_count = 0;
+			while (1) {
+				vc >> frame;
+
+				if (frame.empty())
+					break;
+				else
+				{
+					skip_count++;
+					//cout << skip_count << endl;
+					waitKey((int)(1000 / fps_origin));
+					if (skip_count == (int)fps_origin / TARGET_FPS) {
+						// imshow("video", frame);
+						string save_file_path = SAVE_LOAD_PATH + string("\\") + filename + "_" + to_string(save_count) + string(".jpg");
+						ReplaceAll(save_file_path, " ", "_");
+						cout << imwrite(save_file_path, frame) << endl;
+						cout << save_file_path << endl;
+						skip_count = 0;
+						save_count++;
+					}
+				}
+			}
+		}
+	}
+	
+	if (TXT_FLAG) {
+		int i = 0;
+
+		vector<string> data_list;
+		for (auto& p1 : std::experimental::filesystem::directory_iterator(SAVE_LOAD_PATH)) {
+			string filename = p1.path().string();
+			string data_name;
+			istringstream ss(filename);
+			while (getline(ss, data_name, '\\')) {}
+			data_list.push_back("data/img/" + data_name + "\n");
+			filename = change_file_extention(filename, string("txt"));
+			string txt_path = SAVE_LOAD_PATH + string("\\") + filename;
+			ofstream of(txt_path);
+
+		}
+
+		write_file(SAVE_LOAD_PATH + string("\\train.txt"), data_list);
+	}
+	
+	return 0;
+}
+
+vector<string> get_filepaths_in_directory(string path) {
+	vector<string> paths;
+
+	for (auto& p1 : std::experimental::filesystem::directory_iterator(path)) {
+		paths.push_back(p1.path().string());
+	}
+
+	return paths;
+}
+
+string ReplaceAll(std::string &str, const std::string& from, const std::string& to) {
+	size_t start_pos = 0; //string처음부터 검사
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos)  //from을 찾을 수 없을 때까지
+	{
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // 중복검사를 피하고 from.length() > to.length()인 경우를 위해서
+	}
+	return str;
+}
+
+string change_file_extention(string filename, string extention) {
+	istringstream ss(filename);
+	while (getline(ss, filename, '\\')) {}
+	ss = istringstream(filename);
+	getline(ss, filename, '.');
+	filename = filename + "." + extention;
+	return filename;
+}
+
+void write_file(string path, vector<string> data) {
+	ofstream out_stream(path);
+	for (int idx = 0; idx < data.size(); idx++) {
+		out_stream << data[idx];
+	}
+	out_stream.close();
+}
---
+++ YOLODataNumChecker/Sources/YOLODataNumChecker.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <filesystem>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+//#define PATH "D:\\Users\\VisionWork\\Desktop\\박하연\\Projects\\TrainingDataGenerator\\YOLODataRefiner\\YOLODataRefiner\\data\\img"
+#define PATH "D:\\Users\\VisionWork\\Desktop\\박하연\\DB\\final"
+#define TOTAL 1
+
+int main() {
+	vector<int> label_num(23);
+	vector<string> label_names;
+
+	label_names.push_back("Speed OTher");
+	label_names.push_back("Child Protect");
+	label_names.push_back("No Left");
+	label_names.push_back("No Right");
+	label_names.push_back("No Parking");
+	label_names.push_back("Crosswalk");
+	label_names.push_back("No Uturn");
+	label_names.push_back("Rotate");
+	label_names.push_back("Slow");
+	label_names.push_back("Speed Bust");
+	label_names.push_back("Unprotected Left");
+	label_names.push_back("Uturn");
+	label_names.push_back("No Straight");
+	label_names.push_back("Speed 20");
+	label_names.push_back("Speed 30");
+	label_names.push_back("Speed 40");
+	label_names.push_back("Speed 50");
+	label_names.push_back("Speed 60");
+	label_names.push_back("Speed 70");
+	label_names.push_back("Speed 80");
+	label_names.push_back("Speed 90");
+	label_names.push_back("Speed 100");
+	label_names.push_back("ALL");
+
+	int a = 0;
+
+	for (auto& p : std::experimental::filesystem::directory_iterator(PATH)) {
+		string dirname = p.path().string();
+		string filename_1;
+		string trainname = p.path().string() + string("\\train.txt");
+		cout << dirname << endl;
+		vector<string> data;
+
+		stringstream ss_1(dirname);
+		while (getline(ss_1, filename_1, '\\'));
+		if((filename_1.find("19") == std::string::npos)) continue;
+
+		ifstream in_stream(trainname);
+		string input;
+
+		while (!in_stream.eof()) {
+			getline(in_stream, input);
+			data.push_back(input);
+		}
+		in_stream.close();
+
+		for (int i = 0; i < data.size() - 1; i++) {
+			istringstream ss(data[i]);
+			string filename;
+
+			while (getline(ss, filename, '/')) {}
+			ss = istringstream(filename);
+			getline(ss, filename, '.');
+			filename += ".txt";
+			filename = dirname + "\\" + filename;
+
+			vector<string> bbdata;
+
+			ifstream in_stream(filename);
+			string input;
+
+			while (!in_stream.eof()) {
+				getline(in_stream, input);
+				bbdata.push_back(input);
+			}
+			in_stream.close();
+
+			
+			for (int i = 0; i < bbdata.size(); i++) {
+				stringstream ss(bbdata[i]);
+				int label;
+				string s;
+				ss >> s;
+				if (s == string("")) {
+					continue;
+				}
+				label = stoi(s);
+				label_num[label]++;				
+			}
+			label_num.back() += 1;
+		}
+		cout << "OK!" << endl;
+
+		if (TOTAL == 0) {
+			cout << "[ " << filename_1 << " ]" << endl;
+			for (int i = 0; i < label_num.size(); i++) {
+				cout << label_names[i] << " : " << label_num[i] << endl;
+				label_num[i] = 0;
+			}
+		}
+		
+	}
+
+	if (TOTAL == 1) {
+		cout << "[ Total ]" << endl;
+		for (int i = 0; i < label_num.size(); i++) {
+			cout << label_names[i] << " : " << label_num[i] << endl;
+		};
+	}
+	
+
+	return 0;
+}
